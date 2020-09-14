ad707927134160e678e4d1eb4bd19f3feb7d86f7
Rahul Goyal <rahulg@qubole.com>
2020-09-14 09:53:15 +0530

    gofmt to style code

--- main.go
+++ main.go
@@ -1,10 +1,9 @@
-
 package main
 
 import (
-  "fmt"
+	"fmt"
 )
 
 func main() {
-  fmt.Println("Hello, Using go modules to initialize a project..")
+	fmt.Println("Hello, Using go modules to initialize a project..")
 }
